2bdb21b67d4aae3a3247493bccc3c5bcc6c77411
limrod631 <kevin.omwega@gmail.com>
2023-09-28 23:54:23 -0500

    fixed a bug that caused an infinite loop on validateInput

--- Develop/script.js
+++ Develop/script.js
@@ -1,8 +1,28 @@
+//Create my empty arrays
+
+let specialCharOptions = [" ","!","","#","$","%","&","'","(",")","*","+",",","-",".","/",":",";","<","=",">","?","@","[","]","^","_","`","{","|","}","~"];
+let upperOptions = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+let lowerOptions = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+let numsOptions = ['0','1','2','3','4','5','6','7','8','9'];
+let passwordArray = [];
+let randomOptions = []; 
+
+//Create object to store user data
+
+let userOptions = {
+  charLength: Number,
+  charLower: Boolean,
+  charUpper: Boolean,
+  charNums: Boolean,
+  specialChar: Boolean
+};
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+  resetArrays();
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
   passwordText.value = password;
@@ -18,25 +38,6 @@ passwordTextarea.addEventListener("click", function(){
   document.execCommand('copy');
   alert("Password copied to clipboard");
 })
-
-//Create object to store user data
-
-let userOptions = {
-  charLength: Number,
-  charLower: Boolean,
-  charUpper: Boolean,
-  charNums: Boolean,
-  specialChar: Boolean
-};
-
-//Create my empty arrays
-
-let specialCharOptions = [" ","!","","#","$","%","&","'","(",")","*","+",",","-",".","/",":",";","<","=",">","?","@","[","]","^","_","`","{","|","}","~"];
-let upperOptions = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
-let lowerOptions = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
-let numsOptions = ['0','1','2','3','4','5','6','7','8','9'];
-let passwordArray = [];
-let randomOptions = [];
 
 //Generate my password
 
@@ -56,17 +57,24 @@ function generatePassword (){
   return password;
 };
 
+//function to reset arrays
+
+function resetArrays(){
+  passwordArray.length = 0;
+  userOptions.length = 0;
+  randomOptions.length = 0;
+}
 
 //Get user input on the length of the password
 
 function getLength(){
 
-  var chooseLength = window.prompt("How many numbers do you want to use between 2 and 128?");
+  var chooseLength = window.prompt("How many characters do you want to use between 2 and 128?");
   userOptions.charLength=chooseLength;
 
   while (chooseLength < 2 || chooseLength > 128 || isNaN(chooseLength)) {
     alert("Please enter a valid number!");
-    var chooseLength = window.prompt("How many numbers do you want to use between 2 and 128?");
+    var chooseLength = window.prompt("How many characters do you want to use between 2 and 128?");
     userOptions.charLength=chooseLength;
   }
 
@@ -80,40 +88,44 @@ function getLength(){
 function getCharacters(){
 
   //add lower characters to the options array
-  var chooseLower = window.confirm("Lower?");
+  var chooseLower = window.confirm("Do you want lowercase characters?");
   userOptions.charLower=chooseLower;
   
   if (chooseLower){
     randomOptions.push(...lowerOptions);
   } 
   console.log(randomOptions);
+  console.log(userOptions);
 
   //add upper characters to the options array
-  var chooseUpper = window.confirm("Upper?");
+  var chooseUpper = window.confirm("Do you want uppercase characters?");
   userOptions.charUpper=chooseUpper;
   
   if (chooseUpper){
     randomOptions.push(...upperOptions);
   }
   console.log(randomOptions);
+  console.log(userOptions);
 
   //add numbers to the options array
-  var chooseNums = window.confirm("Numbers?");
+  var chooseNums = window.confirm("Do you want numbers?");
   userOptions.charNums=chooseNums;
   
   if (chooseNums){
     randomOptions.push(...numsOptions);
   }
   console.log(randomOptions);
+  console.log(userOptions);
   
   //add characters to the options array
-  var chooseChars = window.confirm("Characters?");
+  var chooseChars = window.confirm("Do you want special characters?");
   userOptions.specialChar=chooseChars;
 
   if (chooseChars){
     randomOptions.push(...specialCharOptions);
   }
   console.log(randomOptions);
+  console.log(userOptions);
 
   return randomOptions;
   
@@ -121,16 +133,9 @@ function getCharacters(){
 
 //Validate that the input the user provided is valid
 function validateInput(){
-    let countTrue = 0;
+    // let countTrue = 0;
 
-    for (let key in userOptions) {
-      if (userOptions[key] === true){
-        countTrue++;
-      }
-    }
-
-    while (countTrue < 1) {
+    if (randomOptions.length < 1) {
       alert("Please try again!");
-      getCharacters();
     } 
   }
